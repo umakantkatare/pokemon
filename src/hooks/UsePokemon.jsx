a4fb1b16fca7a2c6e33900b561458cd882fa94aa
Umakant Katare <umakantkatare9753121329@gmail.com>
2024-02-03 17:23:07 +0530

    first commit

--- src/hooks/UsePokemon.jsx
+++ src/hooks/UsePokemon.jsx
@@ -1,11 +1,26 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function UsePokemon() {
+  const [pokemonList, setPokemonList] = useState([]);
+
   async function downloadPokemon() {
     const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
-    console.log(response.data);
+    const pokemonResults = response.data.results;
+    // console.log(response.data);
+    // console.log(pokemonResults);
+    const pokemonPromise = pokemonResults.map((pokemon) => {
+      axios.get(pokemon.url);
+    });
+    const pokemonData = await axios.all(pokemonPromise);
+    console.log(pokemonData);
+
+    const res = pokemonData.map((pokeData) => {
+      pokeData.data;
+    });
+    console.log(res);
+    setPokemonList(res);
   }
   useEffect(() => {
     downloadPokemon();
